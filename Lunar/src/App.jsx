1578411166088a387d09f11997d96892f96321de
satoshikemboi <satoshikemboi@gmail.com>
2025-12-04 21:26:43 +0300

    Initial commit

---
+++ Lunar/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home.jsx"
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home/>
+    </>
+  );
+}
+
+export default App;
+
+
